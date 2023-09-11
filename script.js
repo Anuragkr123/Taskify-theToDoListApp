4e6991a364e414f29987c9ed46c8e9b75c7e8fec
Anuragkr123 <anuragkashyav1011@gmail.com>
2023-09-11 15:13:09 +0530

    Taskify-theToDoListApp

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const AddTask = document.querySelector(".row input");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+function handleKeypress(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+}
+AddTask.addEventListener("keypress", handleKeypress);
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
